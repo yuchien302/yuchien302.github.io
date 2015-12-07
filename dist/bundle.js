1fe4d35b16510257562a4d4e5c63fb9a8b7606cd
yuchien <yc2257@cornell.edu>
2015-12-07 00:54:31 -0500

    v0.3.1

--- dist/bundle.js
+++ dist/bundle.js
@@ -31804,17 +31804,20 @@ var App = React.createClass({displayName: "App",
     };
   },
   getInitialState: function() {
-    return {slideIndex: 0, sticky: false};
+    return {slideIndex: 0, sticky: false, prevScrollTop: 0};
+  },
+  shouldComponentUpdate: function(nextProps, nextState){
+    return (this.state.slideIndex !== nextState.slideIndex) || (this.state.sticky !== nextState.sticky)
   },
   onChangeTabs: function(value) {
     var index = parseInt(value, 10)
-    var sticky = ReactDOM.findDOMNode(this.refs["swipeview"]).children[0].children[index].scrollTop > 62
+    var sticky = ReactDOM.findDOMNode(this.refs["swipeview"]).children[0].children[index].scrollTop > 10
     this.setState({
       slideIndex: parseInt(value, 10), sticky: sticky
     });
   },
   onChangeIndex: function(index) {
-    var sticky = ReactDOM.findDOMNode(this.refs["swipeview"]).children[0].children[index].scrollTop > 62
+    var sticky = ReactDOM.findDOMNode(this.refs["swipeview"]).children[0].children[index].scrollTop > 10
     this.setState({
       slideIndex: index, sticky: sticky
     });
@@ -31825,19 +31828,24 @@ var App = React.createClass({displayName: "App",
 
   componentWillUnmount: function() {
     document.documentElement.removeEventListener('scroll', this.handleScroll);
-  },  
+  },
+
   onScrollSlide: function(e){
-    // console.log(e.target.scrollTop)
-    if(e.target.scrollTop > 62 && !this.state.sticky){
+    var scrollTop = e.target.scrollTop;
+
+    if( scrollTop > 10 && (scrollTop - this.state.prevScrollTop) > 0 && !this.state.sticky){
       this.setState({
-        sticky: true
+        sticky: true, prevScrollTop: scrollTop
       });
-    } else if(e.target.scrollTop <= 62 && this.state.sticky){
+    } else if( scrollTop <= 100 && (scrollTop - this.state.prevScrollTop) < 0 && this.state.sticky){
       this.setState({
-        sticky: false
+        sticky: false, prevScrollTop: scrollTop
+      });
+    } else{
+      this.setState({
+        prevScrollTop: scrollTop
       });
     }
-    
   },
   render: function() {
     if(this.state.sticky){
@@ -32137,6 +32145,7 @@ var ProjectBaseSystem = React.createClass({displayName: "ProjectBaseSystem",
           this.props, 
           {show: this.state.show, 
           onHide: this.hideModal, 
+          animation: false, 
           dialogClassName: "custom-modal"
         }), 
           React.createElement(Modal.Header, {closeButton: true}, 
@@ -32150,15 +32159,7 @@ var ProjectBaseSystem = React.createClass({displayName: "ProjectBaseSystem",
              "Quas laudantium laudantium modi corporis nihil provident consectetur omnis, natus nulla distinctio illum corporis. Sit ex earum odio ratione consequatur odit minus laborum? Eos? Sit ipsum illum architecto aspernatur perspiciatis error fuga illum, tempora harum earum, a dolores. Animi facilis inventore harum dolore accusamus" + ' ' +
              "fuga provident molestiae eum! Odit dicta error dolorem sunt reprehenderit. Sit similique iure quae obcaecati harum. Eum saepe fugit magnam dicta aliquam? Sapiente possimus aliquam fugiat officia culpa sint! Beatae voluptates voluptatem excepturi molestiae alias in tenetur beatae placeat architecto. Sit possimus rerum" + ' ' +
              "fugiat sapiente aspernatur. Necessitatibus tempora animi dicta perspiciatis tempora a velit in! Doloribus perspiciatis doloribus suscipit nam earum. Deleniti veritatis eaque totam assumenda fuga sapiente! Id recusandae. Consectetur necessitatibus eaque velit nobis aliquid? Fugit illum qui suscipit aspernatur alias ipsum" + ' ' +
-             "repudiandae! Quia omnis quisquam dignissimos a mollitia. Suscipit aspernatur eum maiores repellendus ipsum doloribus alias voluptatum consequatur. Consectetur quibusdam veniam quas tenetur necessitatibus repudiandae? Rem optio vel alias neque optio sapiente quidem similique reiciendis tempore. Illum accusamus officia" + ' ' +
-             "cum enim minima eligendi consectetur nemo veritatis nam nisi! Adipisicing nobis perspiciatis dolorum adipisci soluta architecto doloremque voluptatibus omnis debitis quas repellendus. Consequuntur assumenda illum commodi mollitia asperiores? Quis aspernatur consequatur modi veritatis aliquid at? Atque vel iure quos." + ' ' +
-             "Amet provident voluptatem amet aliquam deserunt sint, elit dolorem ipsa, voluptas? Quos esse facilis neque nihil sequi non? Voluptates rem ab quae dicta culpa dolorum sed atque molestias debitis omnis! Sit sint repellendus deleniti officiis distinctio. Impedit vel quos harum doloribus corporis. Laborum ullam nemo quaerat" + ' ' +
-             "reiciendis recusandae minima dicta molestias rerum. Voluptas et ut omnis est ipsum accusamus harum. Amet exercitationem quasi velit inventore neque doloremque! Consequatur neque dolorem vel impedit sunt voluptate. Amet quo amet magni exercitationem libero recusandae possimus pariatur. Cumque eum blanditiis vel vitae" + ' ' +
-             "distinctio! Tempora! Consectetur sit eligendi neque sunt soluta laudantium natus qui aperiam quisquam consectetur consequatur sit sint a unde et. At voluptas ut officiis esse totam quasi dolorem! Hic deserunt doloribus repudiandae! Lorem quod ab nostrum asperiores aliquam ab id consequatur, expedita? Tempora quaerat" + ' ' +
-             "ex ea temporibus in tempore voluptates cumque. Quidem nam dolor reiciendis qui dolor assumenda ipsam veritatis quasi. Esse! Sit consectetur hic et sunt iste! Accusantium atque elit voluptate asperiores corrupti temporibus mollitia! Placeat soluta odio ad blanditiis nisi. Eius reiciendis id quos dolorum eaque suscipit" + ' ' +
-             "magni delectus maxime. Sit odit provident vel magnam quod. Possimus eligendi non corrupti tenetur culpa accusantium quod quis. Voluptatum quaerat animi dolore maiores molestias voluptate? Necessitatibus illo omnis laborum hic enim minima! Similique. Dolor voluptatum reprehenderit nihil adipisci aperiam voluptatem soluta" + ' ' +
-             "magnam accusamus iste incidunt tempore consequatur illo illo odit. Asperiores nesciunt iusto nemo animi ratione. Sunt odit similique doloribus temporibus reiciendis! Ullam. Dolor dolores veniam animi sequi dolores molestias voluptatem iure velit. Elit dolore quaerat incidunt enim aut distinctio. Ratione molestiae laboriosam" + ' ' +
-             "similique laboriosam eum et nemo expedita. Consequuntur perspiciatis cumque dolorem.")
+             "repudiandae! Quia omnis quisquam dignissimos a mollitia. Suscipit")
           ), 
           React.createElement(Modal.Footer, null, 
             React.createElement(FlatButton, {onClick: this.hideModal}, "Close")
